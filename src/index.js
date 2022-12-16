00c38378d9966075c1262cd13bd49b42d2701e37
AndreiKolosov <AndreiKolosov@users.noreply.github.com>
2022-12-16 14:10:43 +0200

    feat: create luminance func, delete chroma lib

--- src/index.js
+++ src/index.js
@@ -21,30 +21,95 @@ document.addEventListener('click', (e) => {
 
 });
 
-// function generateRandomColor() {
-//   //RGB
-//   // R #FF0000
-//   // G #00FF00
-//   // B #0000FF
+function hexToRGB(h) {
+  let r = 0,
+    g = 0,
+    b = 0;
 
-//   const hexCodes = '0123456789ABCDEF';
-//   let hexColorCode = '#';
+  // 3 digits
+  if (h.length == 4) {
+    r = '0x' + h[1] + h[1];
+    g = '0x' + h[2] + h[2];
+    b = '0x' + h[3] + h[3];
 
-//   for (let i = 0; i < 6; i++) {
-//     hexColorCode += hexCodes[Math.floor(Math.random() * hexCodes.length)];
-//   }
+    // 6 digits
+  } else if (h.length == 7) {
+    r = '0x' + h[1] + h[2];
+    g = '0x' + h[3] + h[4];
+    b = '0x' + h[5] + h[6];
+  }
 
-//   return hexColorCode;
-// }
+  // returns rgb color in format 'rgb(r,g,b)'
+  // return 'rgb(' + +r + ',' + +g + ',' + +b + ')'; 
+
+  return [+r, +g, +b];
+}
+
+function rgbToHsl(rgb) {
+
+  let [ r, g, b ] = rgb;
+
+  (r /= 255), (g /= 255), (b /= 255);
+  var max = Math.max(r, g, b),
+    min = Math.min(r, g, b);
+  var h,
+    s,
+    l = (max + min) / 2;
+
+  if (max == min) {
+    h = s = 0; // achromatic
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = (b - r) / d + 2;
+        break;
+      case b:
+        h = (r - g) / d + 4;
+        break;
+    }
+    h /= 6;
+  }
+
+  return [h, s, l];
+}
+
+function calcColorLuminance (hexColor) {
+  const rgb = hexToRGB(hexColor); // [r, g, b]
+  const hsl = rgbToHsl(rgb); // [h, s, l]
+
+  return hsl[2]; // l from hsl - lightness
+}
+
+
+function generateRandomColor() {
+  //RGB
+  // R #FF0000
+  // G #00FF00
+  // B #0000FF
+
+  const hexCodes = '0123456789ABCDEF';
+  let hexColorCode = '#';
+
+  for (let i = 0; i < 6; i++) {
+    hexColorCode += hexCodes[Math.floor(Math.random() * hexCodes.length)];
+  }
+
+  return hexColorCode;
+}
 
 function setTextColor(textElement, shade) {
-  const luminance = chroma(shade).luminance();
+  const luminance = calcColorLuminance(shade);
 
   textElement.style.color = luminance > 0.5 ? '#000' : '#FFF';
 }
 
 function setBcgColor(element, shade) {
-  const luminance = chroma(shade).luminance();
+  const luminance = calcColorLuminance(shade);
 
   element.style.background = luminance > 0.5 ? '#000' : '#FFF';
 }
@@ -72,7 +137,7 @@ function getColorsFromHash() {
 }
 
 function setTitleColors() {
-  pageTitle.style.background = `linear-gradient(45deg, ${chroma.random()}, ${chroma.random()}, ${chroma.random()})`;
+  pageTitle.style.background = `linear-gradient(45deg, ${generateRandomColor()}, ${generateRandomColor()}, ${generateRandomColor()})`;
   pageTitle.style.webkitBackgroundClip = 'text';
   pageTitle.style.webkitTextFillColor = 'transparent'
 }
@@ -84,7 +149,7 @@ function setRandomColors(isInitial) {
     const isLocked = column.querySelector('.matcher__btn-icon').classList.contains('lock');
     const codeElement = column.querySelector('.matcher__color-code');
     const lockElement = column.querySelector('.matcher__btn-icon');
-    const randomColor = (colors[index] && isInitial) ? colors[index] : chroma.random();
+    const randomColor = colors[index] && isInitial ? colors[index] : generateRandomColor();
 
     if (isLocked) {
       colors.push(codeElement.textContent);
